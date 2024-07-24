930ad31d03dda992c0da64d09caee4bb6b6e8ee7
mgordeev <mgordeev@viantinc.com>
2024-07-24 11:04:26 -0700

    run test coverage

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
 
 func Add(a, b int) int {
     return a + b
 }
 
+func Subtract(a, b int) int {
+    return a - b
+}
+
+func Multiply(a, b int) int {
+    return a * b
+}
+
+func Divide(a, b int) (int, error) {
+    if b == 0 {
+        return 0, errors.New("division by zero")
+    }
+    return a / b, nil
+}
+
 func main() {
-    fmt.Println("Hello, World!!!!")
+    fmt.Println("Hello, World!")
 }
